d70d5e4bcbdfb0c93143c9aa7d03e900126e9bd2
LuanRM1 <luan.mello@sou.inteli.edu.br>
2023-05-03 12:46:50 -0300

    Servidor funcionando
    Arrumei os erros do servidor

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express');
-app = express();
+const app = express();
 app.use(express.static('public'));
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.sendFile(__dirname + '/Frontend/home.html');
+    res.sendFile(__dirname+'/src/Frontend/home.html');
+
 });
-
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
-
 
