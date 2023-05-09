0e09010f6b7733aafcac76258c5d0998e9a25043
LuanRM1 <luan.mello@sou.inteli.edu.br>
2023-05-09 11:23:10 -0300

    mais endpoints e add banco de dados

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname+'/src/Frontend/home.html');
 
 });
+
+app.get('/login', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.sendFile(__dirname+'/src/Frontend/login.html');
+
+});
+
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
