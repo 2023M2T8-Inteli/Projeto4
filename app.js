76cbe53db463601e907b61a0d8ceecefc462ab4f
Rodrigo Sales <freirerodrigosales@gmail.com>
2023-06-03 23:52:05 -0300

    Opção de seleção do choque 1 e choque 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 const sqlite3 = require('sqlite3').verbose();
 const DBPATH = 'Banco_de_dados/dbProjeto.db';
 var bodyParser = require('body-parser');
@@ -74,7 +74,7 @@ app.get('/choque2', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     //res.sendFile(__dirname+'/src/Frontend/choques.html');
     var id_choque2 = req.query.id;
-    var sql = `SELECT * FROM Choque2 WHERE id_choque2=${id_choque2}`;
+    var sql = `SELECT * FROM Choque2`;
     db.all(sql, [], (err, rows) => {
         if (err) {
             throw err;
