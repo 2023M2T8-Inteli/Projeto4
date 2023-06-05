d102a7c90c589e60889f6f94bfc1215337449b10
LuanRM1 <luan.mello@sou.inteli.edu.br>
2023-06-05 11:42:08 -0300

    retirada de end points inuteis
    assdadsa

--- app.js
+++ app.js
@@ -121,78 +121,7 @@ app.get('/viagens', (req, res) => {
     });
 }); //seleciona todos os dados da tabela Viagem nos quais o id_viagem tem valor igual a um selecionado anteriormente. 
 
-app.post('/inserePico', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    var id_pico = req.body.id_pico;
-    var id_viagem = req.body.id_viagem;
-    var tipo_vagao = req.body.tipo_vagao;
-    var data_hora = req.body.data_hora;
-    var latitude = req.body.latitude;
-    var longitude = req.body.longitude;
-    var velocidade = req.body.velocidade;
-    var posicao = req.body.posicao;
-    var placa_virtual = req.body.placa_virtual;
-    var trecho = req.body.trecho;
-    var engate = req.body.engate;
-    var delta = req.body.delta;
-    var act = req.body.act;
-    var peg = req.body.peg;
-    sql = `INSERT INTO Pico VALUES (${id_pico}, ${id_viagem}, "${tipo_vagao}", ${data_hora}, ${latitude}, ${longitude}, ${velocidade}, ${posicao}, "${placa_virtual}", "${trecho}", ${engate}, ${delta}, ${act}, ${peg})`;
-    db.run(sql, [], err => {
-        if (err) {
-            res.send("Erro na gravação: " + err);
-        }
-        else {
-            res.send("Pico cadastrado com sucesso");
-        }
-    });
-}); //insere na tabela Pico valores como id_pico, id_viagem, tipo do vagão, etc.
 
-
-app.post("/deletPico", (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    var id_pico = parseInt(req.body.id_pico);
-    console.log(parseInt(id_pico))
-    var sql = `DELETE FROM Pico WHERE id_pico = ${id_pico}`;
-    //console.log(sql,'hahahahah');
-    db.run(sql, [], (err,rows) => {
-        if(err){
-            throw err;  
-        }
-        console.log("Registro deletado com sucesso");
-        console.log(rows)
-    });
-}); //deleta todos os valores que tenham deterinado id na tabela pico.
-
-app.put('/atualizaPico', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    var id_pico = req.body.id_pico
-    var tipo_vagao = req.body.tipo_vagao;
-    var data_hora = req.body.data_hora;
-    var latitude = req.body.latitude;
-    var longitude = req.body.longitude;
-    var velocidade = req.body.velocidade;
-    var posicao = req.body.posicao;
-    var placa_virtual = req.body.placa_virtual;
-    var trecho = req.body.trecho;
-    var engate = req.body.engate;
-    var delta = req.body.delta;
-    var act = req.body.act;
-    var peg = req.body.peg;
-    var sql = `UPDATE Pico SET tipo_vagao="${tipo_vagao}", data_hora=${data_hora}, latitude=${latitude}, longitude=${longitude}, velocidade=${velocidade}, posicao=${posicao}, placa_virtual="${placa_virtual}", trecho="${trecho}", engate=${engate}, delta=${delta}, act=${act}, peg=${peg} WHERE id_pico = ${id_pico}`;
-    db.run(sql, [], (err,rows) => {
-        if (err) {
-            res.send("Erro na atualização: " + err);
-        }
-        else {
-            res.send("Pico atualizado com sucesso")
-            console.log(rows);
-        }
-    });
-}); //atualiza os dados relativos a determinado id_pico na tabela Pico.
 
 app.get('/relatorio', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
